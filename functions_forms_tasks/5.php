728a2b11baa24de4717b6742b599f303647f8614
TalisiaDurmanenko <dlareestr@gmail.com>
2016-11-04 18:12:55 +0100

    5.php

--- functions_forms_tasks/5.php
+++ functions_forms_tasks/5.php
@@ -3,16 +3,24 @@
 
 $dir = 'C:\xampp\htdocs\homeworks\functions_forms_tasks';
 $files = scandir($dir);
+//unset($files[0]);
+//unset($files[1]);
 print_r($files);
 echo "Спикок файлів, в яких є слово слово \"файл\":<br> ";
+
+
 foreach ($files as $file){
-    if (strpos(file_get_contents($file), "файл"));
-    {
+    $search = file_get_contents($file);
+    $needle =  "файл";
+    if (strpos($search, $needle)) {
         echo "$file<br>";
+
     }
+
 }
-/*$a = '3.php';
+/*
+$a = '3.php';
 $f = file_get_contents($a);
-if (strpos($f, "файл")){
+if (strpos($f, "фddsf")){
     echo $a;
-}
+}*/
