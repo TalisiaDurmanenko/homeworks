72667940d91344c91b8462f9f789e2ba68ee01be
TalisiaDurmanenko <dlareestr@gmail.com>
2016-11-06 18:11:24 +0100

    homework 07/10

--- functions_forms_tasks/test.php
+++ functions_forms_tasks/test.php
@@ -4,26 +4,23 @@
 
 /*Написать функцию, которая считает количество уникальных слов в тексте.
  Слова разделяются пробелами. Слов. Текст должен вводиться с формы.*/
-$x = "a васька слушает да ест. а воз и ныне там. а вы друзья как ни садитесь, все в музыканты не годитесь. а король-то — голый. а ларчик просто открывался. а там хоть трава не расти.";
-$wordsAr = explode(". ", $x); // повертає масив, значення якого становлять речення тексту
 
-print_r($wordsAr);
-echo "34<br>";
-/*foreach($wordsAr as $sentence){
-    mb_internal_encoding("UTF-8");
-    $Sentence = ucfirst($sentence);
-    //$sentence = mb_strtoupper(mb_substr($sentence, 0, 1, 'UTF-8'), 'UTF-8');
-   //$sentence = mb_substr($sentence, 1, mb_strlen($sentence), 'UTF-8');
-    echo "$Sentence<br>";
-}*/
 
-for($i=0; $i <= count($wordsAr); $i++){
-    mb_internal_encoding("UTF-8");
-    $wordsAr[$i] = ucfirst($wordsAr);
-    echo $wordsAr[$i];
-    echo "<br>";
+function upper($x){
+    $wordsAr = explode(". ", $x); // повертає масив, значення якого становлять речення тексту
+
+    foreach($wordsAr as $sentence){
+        mb_internal_encoding("UTF-8"); // не знаю, що це але без цього з кирилецею не працює
+        //$Sentence = ucfirst($sentence); //чомусь не вдалося зробити за допомогою цієї функції з кириличним текстом
+        $litera = mb_strtoupper(mb_substr($sentence, 0, 1)); // робить великою першу літеру
+        $sentence = $litera . mb_substr($sentence, 1, mb_strlen($sentence)); // з'єднує велику першу літеру з іншим
+        $WordsAr[] = $sentence; // додає нове речення з великою літерою останнім елементом масиву
+    }
+    $words = implode(". ", $WordsAr); // повертає рядок зі значень масиву, розділяючи крапкою і пробілом
+    echo($words);
 }
-//mb_strtoupper(mb_substr($sentence, 0, 1, 'UTF-8'), 'UTF-8')
-//mb_substr(sentence, 1, mb_strlen($sentence), 'UTF-8');
+
+$text = array("а васька слушает да ест. а воз и ныне там. а вы друзья как ни садитесь, все в музыканты не годитесь. а король-то — голый. а ларчик просто открывался.");
+upper($text[0]);
 ?>
 
