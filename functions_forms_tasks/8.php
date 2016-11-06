8c72f429fc628e9629d8f373f10caca87b447e5f
TalisiaDurmanenko <dlareestr@gmail.com>
2016-11-06 10:35:27 +0100

    8.php

--- functions_forms_tasks/8.php
+++ functions_forms_tasks/8.php
@@ -12,8 +12,11 @@
 <?php
 //Написать функцию, которая переворачивает строку. Было "abcde", должна выдать "edcba". Ввод текста реализовать с помощью формы
 
+function reverse($x){
+    echo strrev($x["text"]);
+}
 $string = $_POST;
-print_r($string);
+reverse($string);
 
 ?>
 </body>
